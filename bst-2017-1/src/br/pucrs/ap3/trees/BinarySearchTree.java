bd5312422f4a559360185106566b08283dea4097
10067320 <10067320@l3332111.portoalegre.pucrsnet.br>
2017-04-28 20:01:34 -0300

    Nota de direitos autorais.

--- bst-2017-1/src/br/pucrs/ap3/trees/BinarySearchTree.java
+++ bst-2017-1/src/br/pucrs/ap3/trees/BinarySearchTree.java
@@ -1,5 +1,9 @@
-package br.pucrs.ap3.trees;
+/*
+ * Copyright (c) 2017, ALPRO III and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
 
+package br.pucrs.ap3.trees;
 
 /**
  * 
--- bst-2017-1/src/br/pucrs/ap3/trees/BinarySearchTreeApp.java
+++ bst-2017-1/src/br/pucrs/ap3/trees/BinarySearchTreeApp.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2017, ALPRO III and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
 package br.pucrs.ap3.trees;
 
 public class BinarySearchTreeApp {
