e71f86b38244ba4ff3139ea9124083c405980354
Marco Mangan <marco.mangan@pucrs.br>
2017-05-15 21:05:44 -0300

    Final tests.

--- bst-2017-1/src/br/pucrs/ap3/heaps/Heap.java
+++ bst-2017-1/src/br/pucrs/ap3/heaps/Heap.java
@@ -3,8 +3,17 @@ package br.pucrs.ap3.heaps;
 public class Heap {
 
 	public static boolean isMaxHeap(int[] v) {
-		// TODO verificar se eh um max heap!
-		return false;
+		if (v == null)
+			throw new IllegalArgumentException("Value v must be not null.");
+		for (int i = 0; i <= v.length / 2 - 1; i++) {
+			int left = 2 * i + 1;
+			if (left < v.length && v[i] < v[left])
+				return false;
+			int right = 2 * i + 2;
+			if (right < v.length && v[i] < v[right])
+				return false;
+		}
+		return true;
 	}
 
 }
--- bst-2017-1/src/br/pucrs/ap3/heaps/HeapTest.java
+++ bst-2017-1/src/br/pucrs/ap3/heaps/HeapTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class HeapTest {
 
 	@Test
-	public void testNotMaxHeap() {
+	public void testNotMaxHeap15() {
 		int[] v = { 15, 60, 72, 70, 56, 32, 62, 92, 45, 30, 65 };
 		boolean expected = false;
 		boolean actual = Heap.isMaxHeap(v);
@@ -15,11 +15,48 @@ public class HeapTest {
 	}
 	
 	@Test
-	public void testMaxHeap() {
+	public void testMaxHeap85() {
 		int[] v = { 85, 70, 80, 50, 40, 75, 30, 20, 10, 35, 15, 62, 58 };
 		boolean expected = true;
 		boolean actual = Heap.isMaxHeap(v);
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void testMaxHeap97() {
+		int[] v = { 92, 70, 72, 60, 65, 32, 62, 15, 45, 30, 56 };
+		boolean expected = true;
+		boolean actual = Heap.isMaxHeap(v);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testMaxHeapSingleton() {
+		int[] v = { 15 };
+		boolean expected = true;
+		boolean actual = Heap.isMaxHeap(v);
+		assertEquals(expected, actual);
+	}	
+	
+	@Test
+	public void testMaxHeap65() {
+		int[] v = { 65, 30, 56 };
+		boolean expected = true;
+		boolean actual = Heap.isMaxHeap(v);
+		assertEquals(expected, actual);
+	}	
+	
+	@Test
+	public void testNotMaxHeap65() {
+		int[] v = { 30, 65, 56 };
+		boolean expected = false;
+		boolean actual = Heap.isMaxHeap(v);
+		assertEquals(expected, actual);
+	}	
 
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidArgument() {
+		Heap.isMaxHeap(null);
+	}	
+	
 }
