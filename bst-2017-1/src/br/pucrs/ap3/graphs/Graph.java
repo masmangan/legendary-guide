0da5a4095850bcbf08bf58718dd669f29bae2535
Marco Mangan <marco.mangan@pucrs.br>
2017-05-26 20:31:52 -0300

    Grafos: representacao

---
+++ bst-2017-1/src/br/pucrs/ap3/graphs/Graph.java
@@ -0,0 +1,82 @@
+package br.pucrs.ap3.graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Grafo não-dirigido (Directed graph - Digrafo).
+ * 
+ * @author marco.mangan@pucrs.br
+ *
+ */
+public class Graph {
+
+	private int[][] m;
+
+	public Graph(int size) {
+		if (size <= 0)
+			throw new IllegalArgumentException("Size must be 1 or more.");
+		m = new int[size + 1][size + 1];
+	}
+
+	@Override
+	public String toString() {
+		String r = "    ";
+		for (int j = 1; j < m.length; j++) {
+			r += j + " ";
+		}
+		r += "\n";
+		r += "  +";
+		for (int j = 1; j < m.length; j++) {
+			r += "--";
+		}
+		r += "\n";
+		for (int i = 1; i < m.length; i++) {
+			r += i + " | ";
+			for (int j = 1; j < m.length; j++) {
+				r += m[i][j] + " ";
+			}
+			r += "\n";
+		}
+		return r;
+	}
+
+	public void addEdge(int i, int j) {
+		addEdge(i, j, 1);
+	}
+
+	public void addEdge(int i, int j, int value) {
+		checkNode(i);
+		checkNode(j);
+		m[i][j] = value;
+		m[j][i] = value;
+	}
+
+	private void checkNode(int i) {
+		if (i <= 0 || i >= m.length)
+			throw new IllegalArgumentException("Invalid node:" + i);
+	}
+
+	public List<Integer> getNext(int i) {
+		checkNode(i);
+		List<Integer> r = new ArrayList<>();
+		for (int j = 1; j < m.length; j++) {
+			if (m[i][j] != 0)
+				r.add(j);
+		}
+		return r;
+	}
+	
+	// TODO marcar se foi visitado, para evitar ciclos
+	public List<Integer> breadth(int i) {
+		checkNode(i);
+		// TODO
+		return null;
+	}
+
+	public List<Integer> depth(int i) {
+		checkNode(i);
+		// TODO
+		return null;
+	}
+}
---
+++ bst-2017-1/src/br/pucrs/ap3/graphs/GraphApp.java
@@ -0,0 +1,22 @@
+package br.pucrs.ap3.graphs;
+
+public class GraphApp {
+	public static void main(String[] args) {
+		Graph g = new Graph(5);
+
+		g.addEdge(1, 2);
+		g.addEdge(1, 5);
+
+		g.addEdge(2, 3);
+		g.addEdge(2, 4);
+		g.addEdge(2, 5);
+		
+		g.addEdge(3, 4);
+
+		g.addEdge(4, 5);
+		
+		System.out.println(g);
+		System.out.println(g.getNext(2));
+		
+	}
+}
